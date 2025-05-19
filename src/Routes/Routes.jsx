3586c3387354291d58580fd5ce8179c42431008f
iftekher hossain <gamingalve@gmail.com>
2025-05-19 19:29:17 +0600

    first commit

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,13 +1,18 @@
 import { createBrowserRouter } from "react-router";
 
 import Error from "../Pages/Error";
-import Home from "../Home";
+import Home from "../Pages/Home";
+import AddRecipe from "../Pages/AddRecipe";
 
 const router = createBrowserRouter([
   {
     path: "/",
     ErrorBoundary: Error,
     Component: Home,
+  },
+  {
+    path: "/add-recipe",
+    Component: AddRecipe,
   },
 ]);
 
